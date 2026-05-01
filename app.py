4f273de21afb708fc86fed1eddde33e23432bf07
eledays <leotol1432@gmail.com>
2026-05-01 23:13:08 +0300

    feat: Web App Manifest

--- app.py
+++ app.py
@@ -181,8 +181,13 @@ def create_app():
 
     @app.route('/favicon.ico')
     def favicon():
-        return send_from_directory(os.path.join(app.root_path, 'static'),
+        return send_from_directory(os.path.join(app.root_path, 'static/icons'),
                                    'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+    @app.route('/manifest.json')
+    def manifest():
+        return send_from_directory(os.path.join(app.root_path, 'static/icons'),
+                                   'manifest.json', mimetype='application/json')
 
     return app
 
